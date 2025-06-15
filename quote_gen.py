6358605c27eaa1cef2b4bd6e4d9b4d598e9a9e97
paxxeq <bruzdeguzman@yahoo.com>
2025-06-15 19:18:02 +0800

    Add files via upload

---
+++ quote_gen.py
@@ -0,0 +1,6 @@
+import random
+
+random_quote = random.randint(0, 11)
+
+quotes = ["Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill", "Believe you can and you're halfway there. – Theodore Roosevelt", "Dream big. Start small. Act now. – Robin Sharma", "Less is more.", "Dream big.", "Stay curious.", "Be the change.", "Make it happen.", "Keep moving.", "Just begin.", "Live and learn.", "Create yourself."]
+print("The Quote of the day is: " + quotes[random_quote])
